445369d5f536b8a1bb50d60becbf33dafd2e8700
karlaevelize <k.evelize@gmail.com>
2021-12-20 17:26:43 +0100

    Added auth login and sign up

---
+++ auth/jwt.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m"
+
+function toJWT(data) {
+  return jwt.sign(data, jwtSecret, { expiresIn: "2h" });
+}
+
+function toData(token) {
+  return jwt.verify(token, jwtSecret);
+}
+
+module.exports = { toJWT, toData };
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
+const cors = require("cors")
+
 const productRouter = require("./routers/products")
+const authRouter = require("./routers/auth")
 
 const app = express()
 
 const PORT = 4000
 
+//middlewares
+app.use(cors())
+app.use(express.json());
+
+//routers
+app.use("/auth", authRouter)
 app.use("/products", productRouter)
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
